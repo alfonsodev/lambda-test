1473696e393fa717826eb769a5fbdcce7d1dc3fd
Alfosno <fernandez@ghinwa.me>
2016-05-26 18:37:34 +0200

    testing es7

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-exports.handler = function(event, context) {
+exports.handler = async function(event, context) {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
-    context.succeed(event.key1);  // Echo back the first key value
+    context.succeed('{ ES7 in da house: ' + event.key1 + " } ");  // Echo back the first key value
     // context.fail('Something went wrong');
 };
