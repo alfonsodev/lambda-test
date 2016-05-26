f86e8db00c5a223bbfbaf5b56f672bcdfca34d17
Alfosno <fernandez@ghinwa.me>
2016-05-26 21:50:08 +0200

    trying wich

--- index.js
+++ index.js
@@ -5,34 +5,11 @@ var spawn = require('child_process').spawn
 process.env['PATH'] = process.env['PATH'] + ':/tmp/:' + process.env['LAMBDA_TASK_ROOT']
 
 exports.handler = async function(event, context) {
+  exec('ffmpeg --version', function(code, stdout, stderr) {
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
+    context.succeed('done');  // Echo back the first key value
 
-    var dest = '';
-    try {
-      var ffmpeg = spawn('fmpeg', ['--version']);
-      ffmpeg.stdout.on('data', (data) => {
-        dest = data;
-      });
-
-      ffmpeg.stderr.on('data', (data) => {
-        dest = data;
-      });
-
-      ffmpeg.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    if (code == 0) {
-      context.succeed(dest);  // Echo back the first key value
-    } else {
-      context.succeed(dest);  // Echo back the first key value
-    }
-      });
-    } catch (e) {
-          context.succeed(e);  // Echo back the first key value
-    }
-
-
-    // //console.log('Received event:', JSON.stringify(event, null, 2));
-    // console.log('value1 =', event.key1);
-    // console.log('value2 =', event.key2);
-    // console.log('value3 =', event.key3);
-    // // context.fail('Something went wrong');
+  });
 };
